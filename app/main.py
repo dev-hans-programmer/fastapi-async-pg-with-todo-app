9ae5f183cdf528158c3dddef462666b92008e579
Hasan Ali <hans1998ali@gmail.com>
2025-10-08 00:06:47 +0530

    Added linter ruff

--- app/main.py
+++ app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, status
 from fastapi.responses import JSONResponse
 
+
 app = FastAPI()
 
 
-@app.get("/")
+@app.get('/')
 def root():
     return JSONResponse(
         status_code=status.HTTP_200_OK,
-        content={"message": "Hello User, welcome to todolist server"},
+        content={'message': 'Hello User, welcome to todolist server'},
     )
